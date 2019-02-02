db78813d276b097c4e119eaa09b6f116512c5f1a
Sunyoung Shin <shin@stat.wisc.edu>
2019-02-01 19:12:15 -0600

    test.R update

--- tests/test.R
+++ tests/test.R
@@ -2,91 +2,12 @@ library(atSNP)
 library(BiocParallel)
 library(testthat)
 
-if(FALSE) {
-  ## Import the ENCODE motif library
-  
-  encode_motif <- LoadMotifLibrary("http://compbio.mit.edu/encode-motifs/motifs.txt", tag = ">", transpose = FALSE, field = 1, sep = c("\t", " ", ">"), skipcols = 1, skiprows = 1, pseudocount = 0)
-
-  lines <- readLines("http://compbio.mit.edu/encode-motifs/motifs.txt")
-  title.no <- grep(">", lines)
-  source("~/atsnp_git/atSNP/R/utility.R")
-  title.info <- sapply(lines[title.no], function(x) myStrSplit(x, c(">", " ", "\t")))
-  nfields <- sapply(title.info, length)
-  allnames <- encode_motifinfo <- rep("", length(title.no))
-  for(i in 1:2) {
-    allnames[nfields == i + 1] <- sapply(title.info[nfields == i + 1], function(x) x[i])
-    encode_motifinfo[nfields == i + 1] <- sapply(title.info[nfields == i + 1], function(x) x[i + 1])
-  }
-
-  names(encode_motifinfo) <- names(encode_motif) <- allnames
-
-  system.time(save(encode_motif, encode_motifinfo, file = "~/atsnp_git/atSNP/data/encode_library.rda"))
-  
-  ## Import the JASPAR library
-  jaspar_motif <- LoadMotifLibrary(
-                                   "http://jaspar.genereg.net/html/DOWNLOAD/JASPAR_CORE/pfm/nonredundant/pfm_all.txt",
-                                   tag = ">", skiprows = 1, skipcols = 0, transpose = TRUE, field = 1, 
-                           sep = c(">", "\t", " "), pseudocount = 1)
-  lines <- readLines("http://jaspar.genereg.net/html/DOWNLOAD/JASPAR_CORE/pfm/nonredundant/pfm_all.txt")
-  title.no <- grep(">", lines)
-  source("~/atsnp_git/atSNP/R/utility.R")
-  title.info <- sapply(lines[title.no], function(x) myStrSplit(x, c(">", " ", "\t")))
-  nfields <- sapply(title.info, length)
-  allnames <- jaspar_motifinfo <- rep("", length(title.no))
-  allnames <- sapply(title.info, function(x) x[1])
-  jaspar_motifinfo <- sapply(title.info, function(x) x[2])
-  names(jaspar_motifinfo) <- names(jaspar_motif) <- allnames
-  system.time(save(jaspar_motifinfo, jaspar_motif, file = "~/atsnp_git/atSNP/data/jaspar_library.rda"))
-  
-  # construct the test data set
-  motif_library <- encode_motif
-  system.time(snpInfo <- LoadSNPData("/p/keles/ENCODE-CHARGE/volume2/SNP/hg19_allinfo.bed", nrow = 5))
-  motif_library <- motif_library[1]
-  snp_tbl <- read.table("/p/keles/ENCODE-CHARGE/volume2/SNP/hg19_allinfo.bed", nrow = 5, header = TRUE)[, c("snpid", "a1", "a2", "chr", "snp")]
-  motif_scores <- ComputeMotifScore(motif_library, snpInfo, ncores = 1)
-
-  motif_pval <- ComputePValues(motif_library, snpInfo, motif_scores$motif.scores, ncores = 1)
-  
-  i <- 2
-
-  motif_pval[, pval_ratio := abs(log(pval_ref + 1e-10) - log(pval_snp + 1e-10))]
-  
-  system.time(save(motif_library, snpInfo, snp_tbl, motif_scores, file = "~/atsnp_git/atSNP/data/example.rda"))
-  
-  library(BSgenome.Hsapiens.UCSC.hg38)
-  tbl1 <- read.table("~/atsnp_git/data/gwas_snp1.txt", stringsAsFactors = FALSE)
-  names(tbl1) <- c("chr", "snp", "snpid")
-  tbl1 <- tbl1[grep("rs", tbl1$snpid), ]
-  tbl1$snp <- as.integer(tbl1$snp)
-  tbl1 <- na.omit(tbl1)
-  tbl1$chr <- paste("chr", tbl1$chr, sep = "")
-  half.window.size <- 30
-  ## ad-hocly remove rows with errors
-  
-  tbl <- tbl1[!(tbl1$snp - 30) %in% c(82128489,63678456,82727520,63223070,82727627,79864449,79013943,63731181,81244884,81261958,63666419,81714991,78897749,63786954,79632349,78152892,63717525,78655420,78831605,63037654,63778330,63264538,181168108,63696199,63718204,79813518,81287979,81629755,63686837,63712574,78649567,82450909), ]
-  tbl$chr[tbl$chr == "chr23"] <- "chrX"
-  seqvec <- getSeq(Hsapiens, as.character(tbl$chr), start = tbl$snp - half.window.size, end = tbl$snp + half.window.size, as.character = TRUE)
-
-  codes <- seq(4)
-  names(codes) <- c("A", "C", "G", "T")
-  sequences <- sapply(seqvec, function(x) codes[strsplit(x, "")[[1]]])
-  colnames(sequences) <- tbl$snpid
-  rownames(sequences) <- NULL
-  sequences <- t(na.omit(t(sequences)))
-  transition <- .Call("transition_matrix", sequences, package = "atSNP")
-  prior <- apply(transition, 1, sum)
-  prior <- prior/sum(prior)
-  transition <- transition/apply(transition, 1, sum)
-  names(prior) <- colnames(transition) <- rownames(transition) <- c("A", "C", "G", "T")
-  save(prior, transition, file = "~/atsnp_git/atSNP/data/default_par.rda")
-}
-
 ## process the data
 data(example)
 
 motif_scores <- ComputeMotifScore(motif_library, snpInfo, ncores = 1)
 
-motif_scores <- MatchSubsequence(motif_scores$snp.tbl, motif_scores$motif.scores, ncores = 2, motif.lib = motif_library)
+motif_scores <- MatchSubsequence(motif_scores$snp.tbl, motif_scores$motif.scores, ncores = 1, motif.lib = motif_library)
 
 motif_scores[snpid == "rs2511200" & motif == "ALX3_jolma_DBD_M449", ]
 
